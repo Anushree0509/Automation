d08a8b81af7d1e627378aae7239be7207415ae2d
Anushree0509 <43562394+Anushree0509@users.noreply.github.com>
2018-09-25 12:20:47 +0530

    change in DemoC

--- src/framework/DemoA.java
+++ src/framework/DemoA.java
@@ -10,7 +10,7 @@ public class DemoA {
 	public static void main(String[] args) throws Exception {
 		
 		Workbook w = WorkbookFactory.create(new File("./data/Book1.xlsx"));
-		String s = getSheet("sheet1").getRow(0).getCell(0).toString();
+		String s = w.getSheet("sheet1").getRow(0).getCell(0).toString();
 		System.out.println(s);
 
 	}
