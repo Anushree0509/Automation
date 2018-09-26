7e2c9983b0094553aafbc53232c8d8f64c49908f
Anushree0509 <43562394+Anushree0509@users.noreply.github.com>
2018-09-26 15:37:14 +0530

    Changes in Generic package

---
+++ src/generic/IAutoConst.java
@@ -0,0 +1,11 @@
+package generic;
+
+public interface IAutoConst {
+
+	 public static String chrome_key = "webdriver.chrome.driver";
+	 public static String gecko_key = "webdriver.gecko.driver";
+	 public static String chrome_value = "./driver/chromedriver.exe";
+	 public static String gecko_value = "./driver/geckodriver.exe";
+	 public static String CONFIG_PATH = "./config.properties";
+	 public static String PATH1 ="./result/summary.xlsx";
+}
